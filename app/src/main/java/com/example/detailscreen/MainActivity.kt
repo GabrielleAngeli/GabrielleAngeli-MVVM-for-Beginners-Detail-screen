acaf8695e81b23274893f16e22c9edd780fca597
gabrielleangelipoli <gabrielleangelipoli@gmail.com>
2022-06-01 22:48:46 -0300

    - Columns layouts

--- app/src/main/java/com/example/detailscreen/MainActivity.kt
+++ app/src/main/java/com/example/detailscreen/MainActivity.kt
@@ -3,41 +3,51 @@ package com.example.detailscreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.detailscreen.ui.theme.DetailScreenTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DetailScreenTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier
+                    .verticalScroll(rememberScrollState())
+                    .background(color = Color(0xfff2f2f2))
+                    .fillMaxSize()
+            ) {
+                Image(painter = painterResource(id = R.drawable.happy_meal_small),
+                    contentDescription = "Happy Meal",
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Happy Meal", style = TextStyle(
+                        fontSize = 26.sp
+                    ))
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "800 Calories", style = TextStyle(
+                        fontSize = 17.sp
+                    )
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Text(text = "$5.99", style = TextStyle(
+                        color = Color(0xff85bb65),
+                        fontSize = 17.sp
+                    ))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DetailScreenTheme {
-        Greeting("Android")
     }
 }
